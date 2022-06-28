c224690b1945d15ee73f1c8bdf6ebe6c5510fddb
Yuganshiii <yug2803@gmail.com>
2022-06-28 16:17:56 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,28 @@
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
+app.post("/bmicalculator",function(req,res){
+    var w=Number(req.body.weight);
+    var h=Number(req.body.height);
+    var bmi=w/(h*h);
+    res.send("Your BMI is: "+bmi);
+});
+app.post("/",function(req,res)
+{
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var result=num1+num2;
+    res.send("The result of the calculation is "+ result);
+});
+app.listen(3000,function()
+{
+    console.log("Server running at 3000");
+});
